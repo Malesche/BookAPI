39618847dc4d99a3c5cf89110494071d055283fd
Maria <mariatammik@yahoo.de>
2023-10-09 11:59:52 +0200

    WIP HttpClient Experimente

--- DataCollectionPrototype/Clients/OpenLibraryClient.cs
+++ DataCollectionPrototype/Clients/OpenLibraryClient.cs
@@ -1,7 +1,16 @@
-﻿namespace DataCollectionPrototype.Clients
+﻿using System.Net.Http;
+
+namespace DataCollectionPrototype.Clients
 {
     public class OpenLibraryClient
     {
-        
+        //private readonly HttpClient _httpClient;
+
+        //public OpenLibraryClient(HttpClient httpClient)
+        //{
+        //    _httpClient = HttpClient;
+        //}
+
+        //public async Task<>
     }
 }
