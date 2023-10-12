ba498ef0c4aefd2fd7e7007cbc039a322593acc0
Maria <mariatammik@yahoo.de>
2023-10-12 10:18:28 +0200

    retrieving one book from Open Library and saving some fields to database

--- DataCollectionPrototype/SourceGathering/OpenLibrary/OpenLibraryGatherer.cs
+++ DataCollectionPrototype/SourceGathering/OpenLibrary/OpenLibraryGatherer.cs
@@ -12,15 +12,22 @@ namespace DataCollectionPrototype.SourceGathering.OpenLibrary
             client.BaseAddress = new Uri("https://openlibrary.org/");
             OpenLibraryBook book = null;
 
-            HttpResponseMessage response = await client.GetAsync("books/OL8364844M");
-            //if (response.IsSuccessStatusCode)
-            //{
-            //    book = await response.Content.ReadAsAsync<OpenLibraryBook>();
-            //}
+            HttpResponseMessage response = await client.GetAsync("books/OL8364844M.json");
+            if (response.IsSuccessStatusCode)
+            {
+                book = await response.Content.ReadAsAsync<OpenLibraryBook>();
+
+                Console.WriteLine(book.title);
+            }
 
             return new BookModel[]
             {
-                new BookModel()
+                new BookModel
+                {
+                    Title = book.title,
+                    Isbn13 = book.isbn_13[0],
+                    Isbn = book.isbn_10[0]
+                }
             };
         }
     }
--- DataCollectionPrototype/TargetWriting/LibraryApiWriter.cs
+++ DataCollectionPrototype/TargetWriting/LibraryApiWriter.cs
@@ -1,6 +1,7 @@
 ﻿using DataCollectionPrototype.Core;
 using DataCollectionPrototype.Core.Model;
 using System.Net.Http.Json;
+using LibraryService.Api.Books.ViewModels;
 
 namespace DataCollectionPrototype.TargetWriting
 {
@@ -13,20 +14,43 @@ namespace DataCollectionPrototype.TargetWriting
 
             foreach (var bookModel in data)
             {
-                var viewModel = new
-                {
-                    Title = "Peters lustige C# Geschichten",
-                    Language = "Deutsch",
-                    Isbn = "123",
-                    Isbn13 = "123",
-                    Description = "1234",
-                    CoverUrl = "http://google.com",
-                    BookAuthors = Array.Empty<object>(),
+                //var viewModel = new
+                //{
+                //    Title = "Peters lustige C# Geschichten",
+                //    Language = "Deutsch",
+                //    Isbn = "123",
+                //    Isbn13 = "123",
+                //    Description = "1234",
+                //    CoverUrl = "http://google.com",
+                //    BookAuthors = Array.Empty<object>(),
                     
-                };
-
-               var response = await client.PostAsync("/api/Books", JsonContent.Create(viewModel));
+                //};
+               var response = await client.PostAsync("/api/Books", JsonContent.Create(WriteModelFromBookModel(bookModel)));
             }
+        }
+
+        private static BookWriteViewModel WriteModelFromBookModel(BookModel bookModel)
+        {
+            return new BookWriteViewModel()
+            {
+                Title = bookModel.Title,
+                //Format = bookModel.Format,
+                Language = bookModel.Language,
+                Isbn = bookModel.Isbn,
+                Isbn13 = bookModel.Isbn13,
+                Description = bookModel.Description,
+                PubDate = bookModel.PubDate,
+                CoverUrl = bookModel.CoverUrl,
+                WorkId = null,
+                BookAuthors = new List<BookAuthorWriteViewModel>()
+                    //bookModel
+                //    .BookAuthors
+                //    .Select(model => new BookAuthorWriteModel
+                //    {
+                //        AuthorId = model.AuthorId,
+                //        AuthorRole = model.AuthorRole
+                //    }).ToList()
+            };
         }
     }
 }
